65dcc1ab04ae72eed25e54eca1ef0e42dae27b8c
Aditya Kaushik <adityagr8.05@gmail.com>
2025-08-12 00:56:29 +0530

    Converted to serverless

--- server/db/setup-clean.js
+++ server/db/setup-clean.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  ssl: { rejectUnauthorized: false },
 });
 
 async function setupDatabase() {
